9a849febf332464de5b5d283c48b857af89142ba
Deniz Inan <68498418+DenoFury@users.noreply.github.com>
2020-11-01 00:36:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+
+namespace SayiBulmaca
+{
+
+    class Program
+    {
+
+        static void Main(String[] args)
+        {
+            Random r = new Random();
+
+            int dogrusayi = r.Next(0, 100);
+
+
+            bool kazanmak = false;
+
+            do
+            {
+                Console.WriteLine("0 ve 100 arasındaki sayıyı bul");
+                int sayi = int.Parse(Console.ReadLine());
+
+                if (sayi < dogrusayi)
+                {
+                    Console.WriteLine("Çok alçak azcık yüksel .");
+                }
+                else if (sayi > dogrusayi)
+                {
+                    Console.WriteLine("Çok yüksek  alçalmayı dene");
+                }
+                else if (sayi == dogrusayi)
+                {
+                    Console.WriteLine("TEBRIKLER KAZANDIN!!");
+                    kazanmak = true;
+                }
+
+                Console.WriteLine();
+            } while (kazanmak == false);
+            Console.WriteLine("Oynadığın için teşekkürler ");
+            Console.WriteLine("Çıkmak için herhangi bir butona bas.");
+            Console.ReadKey(true);
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
